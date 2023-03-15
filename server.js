328fd5b2efe818dfd4464a5f04170d37a64d692d
Ashton Bowens <ashxgrey11@gmail.com>
2023-03-14 18:49:54 -0700

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require("dotenv").config()
 
 const { DATABASE_URL} = process.env
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const express = require("express")
 const app = express()
 
